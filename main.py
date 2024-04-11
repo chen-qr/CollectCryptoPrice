1081577d278c456878fb4da1030b56730836777e
Alex <alex.chenqr@gmail.com>
2024-04-11 15:09:34 +0800

    add

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import ccxt
 # 从OKX交易所获取数据
 exchange = ccxt.okx()
 markets = exchange.load_markets()
-print(markets)
+# print(markets)
+
+# 获取交易对的价格
+ticker = exchange.fetch_ticker('BTC/USDT')
+print(ticker)
