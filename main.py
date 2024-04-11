e5508ee49f8629855b799d96bb22b648ff995755
Alex <alex.chenqr@gmail.com>
2024-04-11 15:36:53 +0800

    add

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ async def fetch_data(exchange_id):
 
 
 async def collect():
-    collect_exchanges = ["okx", "binance"] # 从两个OKX和币安两个交易所同时获取数据
+    collect_exchanges = ["okx", "binanceus"] # 从两个OKX和币安两个交易所同时获取数据
     tasks = [fetch_data(exchange_id) for exchange_id in collect_exchanges]
     await asyncio.gather(*tasks) # 异步并发处理任务
 
