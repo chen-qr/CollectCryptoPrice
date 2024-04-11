8e4248f25d81e71d6b5bc131a95c09ed7e72ae10
Alex <alex.chenqr@gmail.com>
2024-04-11 17:45:32 +0800

    add

--- DEX_collection.py
+++ DEX_collection.py
@@ -3,4 +3,4 @@ from web3 import Web3
 
 infura_url = 'https://mainnet.infura.io/v3/{}'.format(os.getenv("INFURA_KEY"))
 web3 = Web3(Web3.HTTPProvider(infura_url))
-print(web3.isConnected())
+print(web3.is_connected())
