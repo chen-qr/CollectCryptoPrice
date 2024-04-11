15667dde477cee5b85efb7d2e1396a27625874f3
Alex <alex.chenqr@gmail.com>
2024-04-11 15:32:49 +0800

    add

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-import ccxt
+import asyncio
+import ccxt.async_support as ccxt
 
-# 从OKX交易所获取数据
-exchange = ccxt.okx()
-markets = exchange.load_markets()
-# print(markets)
 
-# 获取交易对的价格
-ticker = exchange.fetch_ticker('BTC/USDT')
-print(ticker)
+async def fetch_data(exchange_id):
+    exchange_class = getattr(ccxt, exchange_id)
+    exchange = exchange_class()
+    try:
+        ticker = exchange.fetch_ticker('BTC/USDT')
+        print(f"{exchange_id} has fetched.")
+        print(ticker)
+    except Exception as e:
+        print(f"{exchange_id} failed to fetch.")
+        print(e)
+    finally:
+        await exchange.close()
+
+
+async def collect():
+    collect_exchanges = ["okx", "binance"] # 从两个OKX和币安两个交易所同时获取数据
+    tasks = [fetch_data(exchange_id) for exchange_id in collect_exchanges]
+    await asyncio.gather(*tasks) # 异步并发处理任务
+
+
+if __name__ == "__main__":
+    asyncio.run(collect())
