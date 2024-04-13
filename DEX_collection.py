a35ead3e1a6ecc5d783efb3b283c5149709e23fe
Alex <alex.chenqr@gmail.com>
2024-04-13 16:50:58 +0800

    add

--- DEX_collection.py
+++ DEX_collection.py
@@ -7,6 +7,6 @@ web3_client = Web3(Web3.HTTPProvider(infura_url))
 print(web3_client.is_connected())
 
 address = Web3.to_checksum_address(uniswap_router_address)
-print(web3_client.eth.getBalance(address))
+print(web3_client.eth.get_balance(address))
 
 uniswap_contract = web3_client.eth.contract(address=address, abi=uniswap_router_abi)
