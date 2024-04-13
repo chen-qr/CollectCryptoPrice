35afabdd03b39d2c88595514b7dac6b49c1d61db
Alex <alex.chenqr@gmail.com>
2024-04-13 17:39:05 +0800

    add

--- src/DEX_collection.py
+++ src/DEX_collection.py
@@ -4,7 +4,7 @@ from uniswap_conofig import uniswap_router_address, uniswap_router_abi
 
 infura_url = 'https://mainnet.infura.io/v3/{}'.format(os.getenv("INFURA_KEY"))
 web3_client = Web3(Web3.HTTPProvider(infura_url))
-assert(web3_client.is_connected() is True, "Con not concect to infura!")
+assert web3_client.is_connected(), "Con not concect to infura!"
 
 address = Web3.to_checksum_address(uniswap_router_address)
 print(web3_client.eth.get_balance(address))
