8ea38552630ae34739effbabf7f862ef60d17780
Alex <alex.chenqr@gmail.com>
2024-04-14 15:46:39 +0800

    add

--- src/DEX_collection.py
+++ src/DEX_collection.py
@@ -9,10 +9,10 @@ assert web3_client.is_connected(), "Con not concect to infura!"
 uniswap_v3_config = json.load(open("config/contracts/uniswap_v3.json"))
 factory_v2_config = json.load(open("config/contracts/factory_v2.json"))
 
-address = Web3.to_checksum_address(uniswap_v3_config.address)
+address = Web3.to_checksum_address(uniswap_v3_config.get("address"))
 assert web3_client.eth.get_balance(address) >= 0, "Balance is not enough!"
 
-uniswap_contract = web3_client.eth.contract(address=address, abi=uniswap_v3_config.ABI)
+uniswap_contract = web3_client.eth.contract(address=address, abi=uniswap_v3_config.get("ABI"))
 
 factoryV2Address = uniswap_contract.functions.factoryV2().call()
-assert factoryV2Address == factory_v2_config.address, "factoryV2Address is not correct!"
+assert factoryV2Address == factory_v2_config.get("address"), "factoryV2Address is not correct!"
