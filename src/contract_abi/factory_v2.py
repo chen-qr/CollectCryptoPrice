60d8ef03f7185b5d3c54501b078eaec7dab0f3f8
Alex <alex.chenqr@gmail.com>
2024-04-14 15:03:17 +0800

    add

--- src/contract_abi/factory_v2.py
+++ src/contract_abi/factory_v2.py
@@ -1,4 +1,4 @@
 import json
 
 factory_v2_address = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
-factory_v2_abi = json.load(open("contract_abi/factory_v2.json"))
+factory_v2_abi = json.load(open("./factory_v2.json"))
--- src/contract_abi/uniswap_v3.py
+++ src/contract_abi/uniswap_v3.py
@@ -1,4 +1,4 @@
 import json
 
 uniswap_v3_address = "0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45"
-uniswap_v3_abi = json.load(open("contract_abi/uniswap_v3.json"))
+uniswap_v3_abi = json.load(open("./uniswap_v3.json"))
