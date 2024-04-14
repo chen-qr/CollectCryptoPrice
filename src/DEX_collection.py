49176060d41210907f63a5c2d1242dbd2b9495ec
Alex <alex.chenqr@gmail.com>
2024-04-14 15:44:34 +0800

    add

--- src/DEX_collection.py
+++ src/DEX_collection.py
@@ -1,16 +1,18 @@
 import os
 from web3 import Web3
-from contract_abi.uniswap_v3 import uniswap_v3_address, uniswap_v3_abi
-from contract_abi.factory_v2 import factory_v2_address, factory_v2_abi
+import json
 
 infura_url = 'https://mainnet.infura.io/v3/{}'.format(os.getenv("INFURA_KEY"))
 web3_client = Web3(Web3.HTTPProvider(infura_url))
 assert web3_client.is_connected(), "Con not concect to infura!"
 
-address = Web3.to_checksum_address(uniswap_v3_address)
+uniswap_v3_config = json.load(open("config/contracts/uniswap_v3.json"))
+factory_v2_config = json.load(open("config/contracts/factory_v2.json"))
+
+address = Web3.to_checksum_address(uniswap_v3_config.address)
 assert web3_client.eth.get_balance(address) >= 0, "Balance is not enough!"
 
-uniswap_contract = web3_client.eth.contract(address=address, abi=uniswap_v3_abi)
+uniswap_contract = web3_client.eth.contract(address=address, abi=uniswap_v3_config.ABI)
 
 factoryV2Address = uniswap_contract.functions.factoryV2().call()
-assert factoryV2Address == factory_v2_address, "factoryV2Address is not correct!"
+assert factoryV2Address == factory_v2_config.address, "factoryV2Address is not correct!"
--- src/contract_abi/factory_v2.py
+++
@@ -1,4 +0,0 @@
-import json
-
-factory_v2_address = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
-factory_v2_abi = json.load(open("src/contract_abi/factory_v2.json"))
